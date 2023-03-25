0e82a8ff336b79ef7653dedc90dca215fc11be01
GouthamGuna <ggowthamsankar@gmail.com>
2023-03-25 23:21:23 +0530

    Learn Nested Routes

---
+++ react-router-demo/src/components/FeaturedProducts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FeaturedProducts() {
+  return (
+    <h1>Featured Products</h1>
+  )
+}
+
+export default FeaturedProducts
---
+++ react-router-demo/src/components/NewProducts.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewProducts = () => {
+  return (
+    <h1>New Products</h1>
+  )
+}
+
+export default NewProducts
