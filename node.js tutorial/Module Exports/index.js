239188f7b0d96ef53968c6957c377249c6d55ecf
GouthamGuna <ggowthamsankar@gmail.com>
2023-04-03 10:53:39 +0530

    initial commit

---
+++ node.js tutorial/Module Exports/index.js
@@ -0,0 +1,10 @@
+const add = require('./add')
+const sub = require('./sub')
+
+console.log('Hello World!')
+
+const sum = add(10, 20);
+console.log('sum of : '+sum);
+
+const subtraction = sub(45,15);
+console.log('subtraction of : '+subtraction);
