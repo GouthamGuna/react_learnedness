5f5a34cdb58b503be6171b33872bc637ae500e67
Goutham <ggowthamsankar@gmail.com>
2023-07-31 17:07:50 +0530

    initial commit

---
+++ freecodecamp/accessing-object-properties-with-variables.js
@@ -0,0 +1,24 @@
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
+console.log(player);
+
+/*
+
+
+
+playerNumber should be a number
+Passed:The variable player should be a string
+Passed:The value of player should be the string Montana
+Passed:You should use bracket notation to access testObj
+Passed:You should not assign the value Montana to the variable player directly.
+Passed:You should be using the variable playerNumber in your bracket notation
+
+*/
