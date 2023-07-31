26b278320a69449b9dd0f58a8e190b402bd76e12
Goutham <ggowthamsankar@gmail.com>
2023-07-31 17:32:27 +0530

    initial commit

---
+++ freecodecamp/add-new-properties-to-a-javascript-object.js
@@ -0,0 +1,16 @@
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+myDog.bark  = "woof";
+
+console.log(myDog)
+
+/*
+
+You should add the property bark to myDog.
+Waiting:You should not add bark to the initialization of myDog.
+*/
