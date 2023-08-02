0cd69c6189dfcfbf5c1cea0c917450f88c3f33d3
Goutham <ggowthamsankar@gmail.com>
2023-08-02 20:10:45 +0530

    initial commit

---
+++ freecodecamp/delete-properties-from-a-javascript-object.js
@@ -0,0 +1,20 @@
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line
+console.log(myDog)
+
+delete myDog.tails;
+
+/*
+
+Waiting:You should delete the property tails from myDog.
+Waiting:You should not modify the myDog setup.
+
+*/
