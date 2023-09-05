012a3741a1799fa2ff5f30b62eb568c65c3ec2ff
Goutham <ggowthamsankar@gmail.com>
2023-09-05 22:39:28 +0530

    Update function.js

--- JavaScript_Functions_Crash_Course/function/function.js
+++ JavaScript_Functions_Crash_Course/function/function.js
@@ -80,3 +80,14 @@ function doSomething() {
 
   inner();
 }
+
+function outer(x) {
+  function inner(y) {
+    return x + y;
+  }
+  return inner;
+}
+
+const outerReturn = outer(10);
+
+outerReturn(2);
