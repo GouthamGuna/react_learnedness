31d074c8405f5e7b058fc34718bd11619126dc1a
Goutham <ggowthamsankar@gmail.com>
2023-09-16 08:37:59 +0530

    initial commit

---
+++ connect_mysql/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password:"",
+    database: ""
+});
+
+app.get("/student", (req, res) => {
+
+    const sql = "select * from student";
+
+    db.query(sql, (err, data) => {
+        if(err) return res.json("Error");
+        return res.json(data);
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Listening...')
+});
