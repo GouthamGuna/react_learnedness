afd7d663e7bb77f922108813e949eb2b07a850b5
GouthamGuna <ggowthamsankar@gmail.com>
2024-01-19 07:09:28 +0530

    initial commit

---
+++ cerpsoft_pro/select_tag_dyn/app.js
@@ -0,0 +1,67 @@
+var numbers = [24, 27, 28, 40, 44, 323, 434, 42];
+
+(function () {
+  const dayNamesvalue = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  let selectElement = document.querySelector("#list");
+
+  dayNamesvalue.forEach((element, index) => {
+    let option = document.createElement("option");
+    option.text = dayNamesvalue[index];
+    option.value = element;
+    selectElement.add(option);
+  });
+
+  /* 
+
+  for (let i = 0; i < dayNamesvalue.length; i++) {
+    let option = document.createElement('option');
+    option.text = dayNamesvalue[i];
+    selectElement.add(option);
+  }
+
+  let selectDiv = document.createElement("div");
+  selectDiv.classList.add("class", "select-tag-ele");
+  selectDiv.setAttribute("id", "select-tag-ele");
+
+  let selectElement = document.createElement("select");
+  selectElement.setAttribute("id", "daysname");
+  
+  let optionElement = document.createElement("option");
+  optionElement.setAttribute('', 'Please Select')
+
+  selectDiv.appendChild(optionElement);
+  divElement.appendChild(selectDiv);
+  */
+})();
+
+// Higher Order Function`s (HOF)
+
+/**
+ * 1.) It`s takes one are more functions as arguments
+ * 2.) It`s may return a function
+ */
+
+function isOdd(arr, returnArr = []) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      returnArr.push(arr[i]);
+    }
+  }
+
+  return returnArr;
+}
+
+const result = isOdd(numbers);
+console.log(result);
+
+const oddArray = numbers.filter((number) => number % 2 !== 0);
+console.log(oddArray);
