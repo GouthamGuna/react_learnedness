1f597f729f6a5464936a84d51e7f2382ed805a25
GouthamGuna <ggowthamsankar@gmail.com>
2024-02-14 22:32:31 +0530

    initial commit

---
+++ cerpsoft_pro/select_tag_dyn/js/data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+      id: "1",
+      question: "what are the use for Loream Ipsum ?",
+      answer:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor dui vitae ante mollis ultricies. Aliquam sollicitudin ipsum non risus cursus, in facilisis lectus fringilla. Mauris ut magna nec sem porta faucibus. Morbi luctus ex vitae dui dictum eleifend. Curabitur congue, lectus eu cursus aliquet, risus ligula posuere purus, sit amet tincidunt neque magna nec est. Fusce congue semper justo, eget eleifend augue tempus in. Vestibulum ullamcorper dignissim sem. Pellentesque libero neque, consequat posuere orci suscipit, mollis porta orci. Suspendisse libero dolor, efficitur vel eros vel, consequat ornare nulla. Pellentesque tristique eros nec mauris fermentum, sit amet gravida augue egestas.",
+    },
+    {
+      id: "2",
+      question: "what are the use for Pellentesque ?",
+      answer:
+        "Pellentesque eu risus ut enim sodales gravida. Mauris non turpis nec neque sollicitudin bibendum. Vestibulum laoreet congue nibh, ut iaculis diam eleifend vitae. Nulla facilisi. Donec ut turpis accumsan, viverra lectus in, interdum lacus. Nunc ornare enim tempor mollis aliquet. Pellentesque in elementum orci. Nam vehicula turpis vulputate volutpat pulvinar. Nullam auctor, lectus quis eleifend convallis, justo nisl semper nunc, nec cursus ex nisi eget ligula. Nunc ut urna odio. Vestibulum pretium felis eget tellus mattis, ac rhoncus tortor rhoncus. Cras vitae pretium ante.",
+    },
+    {
+      id: "3",
+      question: "what are the use for Praesent ?",
+      answer:
+        "Praesent quam sem, gravida eu volutpat eu, tristique id neque. Duis scelerisque odio ut tempor ultricies. Mauris consectetur iaculis nibh, in accumsan velit feugiat vitae. Aliquam nisl libero, porta non nulla in, sagittis ultrices libero. Phasellus efficitur fringilla orci. Donec ac vehicula ligula, nec finibus urna. Nullam laoreet gravida lectus ut facilisis. Proin porttitor efficitur viverra. Nunc elit ante, euismod eget diam aliquet, condimentum posuere nisi. Nulla vitae tempus diam, in imperdiet leo. Suspendisse nec cursus magna.",
+    },
+    {
+      id: "4",
+      question: "what are the use for Pellentesque molestie ?",
+      answer:
+        "Pellentesque molestie arcu vitae purus accumsan, sed bibendum est vulputate. Praesent in tortor odio. In aliquet lectus lacus, vitae congue ligula semper vel. In a quam aliquam, egestas lorem vitae, aliquet sapien. Suspendisse et eros congue, sagittis nunc sit amet, finibus est. Aenean maximus ante eget est posuere, eget faucibus dolor feugiat. Etiam a quam ut est commodo vestibulum. Sed ex dui, accumsan vitae justo eget, posuere volutpat metus. Nulla at varius elit. Fusce accumsan quis ante sit amet vehicula. Aenean eget lectus dolor. Maecenas rhoncus iaculis mauris ullamcorper condimentum. In sit amet orci ut felis pulvinar ultricies. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+    },
+  ];
+  
+  export default data;
